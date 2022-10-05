93c1b265adddd5aa5a3565d846e8c10a3df38af8
Mario Zuniga <mmzv999@gmail.com>
2022-10-05 12:32:11 -0500

    Devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources "saved_properties", only: %i[index create show update destroy]
   resources "properties"
   resources "users", except: %i[index]
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # get 'users/mario'
   # get 'users/show'
   # get 'users/create'
