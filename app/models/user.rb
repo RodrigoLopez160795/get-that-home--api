aa7fa4dcc3507ac129afa56abfa74b3743519766
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-05 13:20:18 -0500

    Add secure token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   # Association
   has_many :properties, dependent: :destroy
   has_many :savedProperties, dependent: :destroy
+  has_secure_password
+  has_secure_token
   # Validation
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/ }
   validates :name, presence: true
   enum role: { landlord: 0, homeseeker: 1 }
-  validates :password, length: { minimum: 6 }, confirmation: true
+  # validates :password, length: { minimum: 6 }, confirmation: true
   validates :email, uniqueness: true, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_180801) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_181737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,8 +79,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_180801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
