ae1400f78f59195f35770ed4692ae2d245605b2b
Mario Zuniga <mmzv999@gmail.com>
2022-10-07 22:15:21 -0500

    Test

--- app/models/property.rb
+++ app/models/property.rb
@@ -22,7 +22,6 @@ class Property < ApplicationRecord
       photos.each_with_object([]) do |photo, array|
         array << Rails.application.routes.url_helpers.rails_blob_url(photo)
       end
-      # Rails.application.routes.url_helpers.rails_blob_url(photos)
     end
   end
 
