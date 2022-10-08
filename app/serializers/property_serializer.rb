3a2e9787515de3f7e717040270a4b13edecaa2d3
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-07 21:12:45 -0500

    Try to fix bug in index properties 3

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,8 +1,7 @@
 class PropertySerializer < ActiveModel::Serializer
-    attributes(
-        :id,:operation_type, :pets, :maintenance, :bathrooms,
-        :bedrooms, :property_type, :price, :about, :address, :active,
-        :user_id, :photos_url
-      )
-  end
-  
+  attributes(
+    :id, :operation_type, :pets, :maintenance, :bathrooms,
+    :bedrooms, :property_type, :price, :about, :address, :active,
+    :user_id
+  )
+end
