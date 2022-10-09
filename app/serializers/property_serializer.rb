333f2fde65b661f1adbdfb85c9913cd8429ee58b
Mario Zuniga <mmzv999@gmail.com>
2022-10-09 11:51:16 -0500

    Area serialized

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,6 +1,6 @@
 class PropertySerializer < ActiveModel::Serializer
   attributes(
-    :id, :operation_type, :pets, :maintenance, :bathrooms,
+    :id, :operation_type, :pets, :maintenance, :bathrooms, :area,
     :bedrooms, :property_type, :price, :about, :address, :active,
     :user_id, :photos_url
   )
