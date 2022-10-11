c42adfa36248c42a9966a5f0cadd63a7210b848f
Mario Zuniga <mmzv999@gmail.com>
2022-10-11 17:22:42 -0500

    google cloud

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :google
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  Rails.application.routes.default_url_options[:host] = "localhost:3000"
+  Rails.application.routes.default_url_options[:host] = "soyunhost"
   # Rails.application.routes.url_helpers.media_url(:host => "localhost:3000")
 
   # Raises error for missing translations.
