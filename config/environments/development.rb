6b31f095d43acd72be9a87134bcfd6858e2c0706
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-12 01:20:53 -0500

    Trying with hosts nil

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.hosts = nil
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :google
 
