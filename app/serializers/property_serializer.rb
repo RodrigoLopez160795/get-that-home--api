9c3220268a2d8ddf8750adcbe5e56d05e615ad2c
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-12 20:28:40 -0500

    Erase photos url for test in frontend

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -2,12 +2,12 @@ class PropertySerializer < ActiveModel::Serializer
   attributes(
     :id, :operation_type, :pets, :maintenance, :bathrooms, :area,
     :bedrooms, :property_type, :price, :about, :address, :active,
-    :user_id, :photos_url
+    :user_id
   )
 
-  def photos_url
-    images = object.images.map do |_image|
-      rails_blob_url(@object.image) if object.images.attached?
-    end
-  end
+  # def photos_url
+  #   images = object.images.map do |_image|
+  #     rails_blob_url(@object.image) if object.images.attached?
+  #   end
+  # end
 end
