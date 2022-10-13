c73c2e7a58f21f219364825468506baf8e5882d2
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-13 10:48:26 -0500

    Fix: try 8

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,4 +1,5 @@
 class PropertySerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes(
     :id, :operation_type, :pets, :maintenance, :bathrooms, :area,
     :bedrooms, :property_type, :price, :about, :address, :active,
