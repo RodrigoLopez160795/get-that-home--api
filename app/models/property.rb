b5894229a986e3c6177ea8024279279f3b71e940
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-13 14:20:28 -0500

    Fix: direct_url try 13

--- app/models/property.rb
+++ app/models/property.rb
@@ -22,7 +22,7 @@ class Property < ApplicationRecord
     return unless photos.attached?
 
     photos.each_with_object([]) do |photo, array|
-      array << Rails.application.routes.url_helpers.rails_blob_path(photo, disposition: "attachment")
+      array << Rails.application.routes.url_helpers.url_for(photo)
     end
     # photos.map(&:url_for)
   end
